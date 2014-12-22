d19812ce107b000cdf66f704e86de4f9730942e8
Chris Davies <chris.davies@theironyard.com>
2014-12-22 18:52:24 -0500

    Live reload

--- gulpfile.js
+++ gulpfile.js
@@ -7,57 +7,64 @@ var gulp = require('gulp'),
   useref = require('gulp-useref'),
   gulpif = require('gulp-if'),
   uglify = require('gulp-uglify'),
-  minifyCss = require('gulp-minify-css');
+  minifyCss = require('gulp-minify-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  connect = require('gulp-connect');
 
+
+// html process html files and minifies js
 gulp.task('html', ['sass'], function ()
 {
   var assets = useref.assets();
 
   return gulp.src('./src/**/*.html')
+    .pipe(plumber())
     .pipe(htmlmin())
     .pipe(assets)
     .pipe(gulpif('*.js', uglify()))
     .pipe(assets.restore())
     .pipe(useref())
-    .pipe(hash_src({ build_dir: './dest', src_path: './src' }))
-    .pipe(gulp.dest('./dest'));
+    .pipe(hash_src({ build_dir: './dist', src_path: './src' }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(connect.reload());
 });
 
+// sass compiles the sass files
 gulp.task('sass', function () {
-  gulp.src('./src/scss/**/*.scss')
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dest/css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(connect.reload());
 });
 
+// clean cleans the dist directory
 gulp.task('clean', function () {
-  return gulp.src('./dest', { read: false })
+  return gulp.src('./dist', { read: false })
     .pipe(clean());
 });
 
-gulp.task('default', ['html']);
+gulp.task('connect', function() {
+  return connect.server({
+    root: './dist',
+    livereload: true
+  });
+});
 
-// gulp-concat
-// gulp-uglify
-// gulp-watch
-// gulp-autoprefixer
-// gulp-plumber
-
-
-//     useref = require('gulp-useref'),
-//     gulpif = require('gulp-if'),
-//     uglify = require('gulp-uglify'),
-//     minifyCss = require('gulp-minify-css');
-//
-// gulp.task('html', function () {
-//     var assets = useref.assets();
-//
-//     return gulp.src('app/*.html')
-//         .pipe(assets)
-//         .pipe(gulpif('*.js', uglify()))
-//         .pipe(gulpif('*.css', minifyCss()))
-//         .pipe(assets.restore())
-//         .pipe(useref())
-//         .pipe(gulp.dest('dist'));
-// });
+// default is the default grunt task
+gulp.task('default', ['connect', 'html'], function () {
+  gulp.watch('./src/**/*.html', ['html']);
+  gulp.watch('./src/**/*.js', ['html']);
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
