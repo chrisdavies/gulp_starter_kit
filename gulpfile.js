afd45ed88ef94224e5d4670b84b395d681530ab2
Chris Davies <chris.davies@theironyard.com>
2014-12-26 13:45:50 -0500

    Adding qunit support

--- gulpfile.js
+++ gulpfile.js
@@ -28,12 +28,16 @@ var gulp = require('gulp'),
   connect = require('gulp-connect'),
   handlebars = require('gulp-compile-handlebars'),
   argv = require('yargs').argv,
-  fs = require('fs');
+  fs = require('fs'),
+  glob = require('glob'),
+  path = require('path'),
+  Zombie = require('zombie'),
+  colors = require('colors/safe');
 
 /*
   Helper functions
 */
-function refreshBrowser () {
+function refreshBrowser() {
   gulp.src(paths.src)
     .pipe(connect.reload());
 }
@@ -42,23 +46,29 @@ function refreshBrowser () {
   html process html/handlebars files, concats, minifies js,
   and hashes the src/href attribute values
 */
-gulp.task('html', ['copy-assests', 'sass', 'vendorjs', 'appjs'], function ()
-{
+gulp.task('html', ['copy-assests', 'sass', 'vendorjs', 'appjs'], function() {
   var partialsPath = paths.src + '/partials',
-    templateData = { },
+    templateData = {},
     templateOptions = {
       ignorePartials: true,
-      partials : { },
-      batch : fs.existsSync(partialsPath) ? [partialsPath] : [],
-      helpers : { }
+      partials: {},
+      batch: fs.existsSync(partialsPath) ? [partialsPath] : [],
+      helpers: {}
     };
 
-  return gulp.src([paths.src + '/**/*.{html,handlebars}', '!' + paths.src + '/partials/**/*'], { base: paths.src })
+  return gulp.src([paths.src + '/**/*.{html,handlebars}', '!' + paths.src + '/partials/**/*'], {
+      base: paths.src
+    })
     .pipe(plumber())
     .pipe(gulpif('*.handlebars', handlebars(templateData, templateOptions)))
-    .pipe(rename({ extname: '.html' }))
+    .pipe(rename({
+      extname: '.html'
+    }))
     .pipe(htmlmin())
-    .pipe(hashSrc({ build_dir: paths.dest, src_path: paths.src }))
+    .pipe(hashSrc({
+      build_dir: paths.dest,
+      src_path: paths.src
+    }))
     .pipe(gulp.dest(paths.dest))
     .pipe(connect.reload());
 });
@@ -73,16 +83,18 @@ function gulpJs(srcPaths, destFileName) {
     .pipe(gulpif(!argv.release, sourcemaps.init()))
     .pipe(concat(destFileName))
     // mangle breaks source maps (a bug w/ uglifier)
-    .pipe(uglify({ mangle: argv.release }))
+    .pipe(uglify({
+      mangle: argv.release
+    }))
     .pipe(gulpif(!argv.release, sourcemaps.write('.')))
     .pipe(gulp.dest(paths.dest + '/js'));
 }
 
-gulp.task('vendorjs', function () {
+gulp.task('vendorjs', function() {
   return gulpJs(paths.src + '/js/vendor/**/*.js', 'vendor.js');
 });
 
-gulp.task('appjs', function () {
+gulp.task('appjs', function() {
   return gulpJs([paths.src + '/js/**/*.js', '!' + paths.src + '/js/vendor/**/*.js'], 'app.js');
 });
 
@@ -93,7 +105,7 @@ gulp.task('appjs', function () {
  with external source maps. Finally, we externalize
  the source maps.
 */
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src(paths.src + '/scss/**/*.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
@@ -104,28 +116,33 @@ gulp.task('sass', function () {
       cascade: false
     }))
     .pipe(postcss([csswring]))
-    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.dest + '/css'));
 });
 
 // clean cleans the dist directory
-gulp.task('clean', function (cb) {
+gulp.task('clean', function(cb) {
   return del([paths.dest + '/*'], cb);
 });
 
 // build cleans, builds, and launches a server
-gulp.task('build', ['clean'], function () {
+gulp.task('build', ['clean'], function() {
   gulp.start('html', 'connect');
 });
 
 // connect sets up the server/live reload shtuffs
 gulp.task('connect', function() {
-  return connect.server({ root: paths.dest, livereload: true });
+  return connect.server({
+    root: paths.dest,
+    livereload: true
+  });
 });
 
 // copy static assets out to the dest directory
-gulp.task('copy-assests', function () {
+gulp.task('copy-assests', function() {
   gulp.src([paths.src + '/fonts/**/*']).pipe(gulp.dest(paths.dest + '/fonts'));
   gulp.src([paths.src + '/img/**/*']).pipe(gulp.dest(paths.dest + '/img'));
 });
@@ -136,8 +153,56 @@ gulp.task('reload-sass', ['sass'], refreshBrowser);
 // reload html/js
 gulp.task('reload-html', ['html'], refreshBrowser);
 
+// qunit runs all qunit tests
+gulp.task('qunit', function(done) {
+  var files = glob.sync('./test/**/*.html');
+  runAllQunits(files);
+
+  function runAllQunits(testFiles) {
+    var browser = new Zombie();
+
+    function printQunitResults() {
+      var passed = browser.document.querySelector('#qunit-testresult .failed').textContent.trim() === '0',
+        color = passed ? 'green' : 'red',
+        text = browser.document.querySelector('#qunit-testresult').textContent;
+
+      console.log(colors[color](text));
+      return text;
+    }
+
+    function cleanUp() {
+      browser.close();
+      done();
+    }
+
+    function testFile(file) {
+      var url = 'file:/' + path.resolve(file);
+
+      console.log('Running ' + file);
+
+      browser.visit(url).then(printQunitResults).then(next);
+    }
+
+    function queueTest() {
+      setTimeout(function () {
+        testFile(testFiles.pop());
+      });
+    }
+
+    function next() {
+      if (testFiles.length) {
+        queueTest();
+      } else {
+        cleanUp();
+      }
+    }
+
+    next();
+  }
+});
+
 // default is what runs when you just type "grunt" with no params
-gulp.task('default', ['build'], function () {
+gulp.task('default', ['build'], function() {
   gulp.watch(paths.src + '/**/*.{js,html,handlebars}', ['reload-html']);
   gulp.watch(paths.src + '/**/*.scss', ['reload-sass']);
 });
