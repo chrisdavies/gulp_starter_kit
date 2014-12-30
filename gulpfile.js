dac3b89d110d7d97213aabe0180578dd9ca7a521
Chris Davies <chris.davies@theironyard.com>
2014-12-30 14:18:16 -0500

    Optimizing the 'debug' build

--- gulpfile.js
+++ gulpfile.js
@@ -1,224 +1,143 @@
 /*
-  Options/parameters
-*/
-var paths = {
-  src: './src',
-  dest: './dist'
-};
+  The gulp build process is as follows:
 
-/*
-  Imports
+  "gulp" runs in debug mode and serves out of the "build" directory
+
+  "gulp release" creates an optimized build in the "release" directory
 */
+
+/* Load plugins */
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
-  postcss = require('gulp-postcss'),
-  concat = require('gulp-concat'),
-  order = require('gulp-order'),
-  csswring = require('csswring'),
-  sourcemaps = require('gulp-sourcemaps'),
-  minifyCss = require('gulp-minify-css'),
-  autoprefixer = require('gulp-autoprefixer'),
-  htmlmin = require('gulp-htmlmin'),
-  del = require('del'),
+  watch = require('gulp-watch'),
+  jshint = require('gulp-jshint'),
+  connect = require('gulp-connect'),
+  cssmin = require('gulp-cssmin'),
+  minifyhtml = require('gulp-minify-html'),
   hashSrc = require('gulp-hash-src'),
-  gulpif = require('gulp-if'),
+  inlinesource = require('gulp-inline-source'),
+  handlebars = require('gulp-static-handlebars'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
-  plumber = require('gulp-plumber'),
-  connect = require('gulp-connect'),
-  handlebars = require('gulp-compile-handlebars'),
-  argv = require('yargs').argv,
-  fs = require('fs'),
-  glob = require('glob'),
-  path = require('path'),
-  Zombie = require('zombie'),
-  colors = require('colors/safe');
+  gulpif = require('gulp-if'),
+  del = require('del'),
+  glob = require('glob');
 
-/*
-  Helper functions
-*/
-function refreshBrowser() {
-  gulp.src(paths.src)
-    .pipe(connect.reload());
-}
-
-/*
-  html process html/handlebars files, concats, minifies js,
-  and hashes the src/href attribute values
-*/
-gulp.task('html', ['copy-assests', 'sass', 'vendorjs', 'appjs'], function() {
-  var partialsPath = paths.src + '/partials',
-    templateData = {},
-    templateOptions = {
-      ignorePartials: true,
-      partials: {},
-      batch: fs.existsSync(partialsPath) ? [partialsPath] : [],
-      helpers: {}
-    };
-
-  return gulp.src([paths.src + '/**/*.{html,handlebars}', '!' + paths.src + '/partials/**/*'], {
-      base: paths.src
-    })
-    .pipe(plumber())
-    .pipe(gulpif('*.handlebars', handlebars(templateData, templateOptions)))
-    .pipe(rename({
-      extname: '.html'
-    }))
-    .pipe(htmlmin())
-    .pipe(hashSrc({
-      build_dir: paths.dest,
-      src_path: paths.src
-    }))
-    .pipe(gulp.dest(paths.dest))
-    .pipe(connect.reload());
-});
-
-/*
-  gulpJs concatenates JS files in the specified
-  source directories, and outputs them to the
-  specified file name.
-*/
-function gulpJs(srcPaths, destFileName) {
-  return gulp.src(srcPaths)
-    .pipe(gulpif(!argv.release, sourcemaps.init()))
-    .pipe(concat(destFileName))
-    // mangle breaks source maps (a bug w/ uglifier)
-    .pipe(uglify({
-      mangle: argv.release
-    }))
-    .pipe(gulpif(!argv.release, sourcemaps.write('.')))
-    .pipe(gulp.dest(paths.dest + '/js'));
-}
-
-gulp.task('vendorjs', function() {
-  return gulpJs(paths.src + '/js/vendor/**/*.js', 'vendor.js');
-});
-
-gulp.task('appjs', function() {
-  return gulpJs([paths.src + '/js/**/*.js', '!' + paths.src + '/js/vendor/**/*.js'], 'app.js');
-});
-
-/*
- sass compiles the sass/scss files.
- It first creates an internal source map, because
- autoprefixer and csswring don't seem to work well
- with external source maps. Finally, we externalize
- the source maps.
-*/
-gulp.task('sass', function() {
-  return gulp.src(paths.src + '/scss/**/*.scss')
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(postcss([csswring]))
-    .pipe(sourcemaps.init({
-      loadMaps: true
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.dest + '/css'));
-});
-
-// clean cleans the dist directory
-gulp.task('clean', function(cb) {
-  return del([paths.dest + '/*'], cb);
-});
-
-// build cleans, builds, and launches a server
-gulp.task('build', ['clean'], function() {
-  gulp.start('html', 'connect');
-});
-
-// connect sets up the server/live reload shtuffs
-gulp.task('connect', function() {
-  return connect.server({
-    root: paths.dest,
+gulp.task('serveBuild', function() {
+  connect.server({
+    root: 'build',
+    port: 8000,
     livereload: true
   });
 });
 
-// copy static assets out to the dest directory
-gulp.task('copy-assests', function() {
-  gulp.src([paths.src + '/fonts/**/*']).pipe(gulp.dest(paths.dest + '/fonts'));
-  gulp.src([paths.src + '/img/**/*']).pipe(gulp.dest(paths.dest + '/img'));
+gulp.task('serveRelease', function() {
+  connect.server({
+    root: 'release',
+    port: 8000
+  });
 });
 
-// reload sass
-gulp.task('reload-sass', ['sass'], refreshBrowser);
+/* Copy static assets to the build directory */
+gulp.task('assets', function() {
+  gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./build/fonts'));
 
-// reload html/js
-gulp.task('reload-html', ['html'], refreshBrowser);
-
-// qunit runs all qunit tests
-// qunit runs all qunit tests
-gulp.task('qunit', function(done) {
-  var files = glob.sync('./test/**/*.html');
-  runAllQunits(files);
-
-  function runAllQunits(testFiles) {
-    var browser = new Zombie();
-
-    function errorTrace(errorNode, selector) {
-      console.log(colors.yellow((errorNode.querySelector(selector) || {}).textContent));
-    }
-
-    function printErrors() {
-      var errors = browser.document.querySelectorAll('#qunit-tests > .fail');
-      Array.prototype.slice.call(errors, 0).forEach(function (error) {
-        errorTrace(error, '.test-name');
-        errorTrace(error, '.test-message');
-        errorTrace(error, '.test-source');
-      });
-    }
-
-    function printQunitResults() {
-      var passed = browser.document.querySelector('#qunit-testresult .failed').textContent.trim() === '0',
-        color = passed ? 'green' : 'red',
-        text = browser.document.querySelector('#qunit-testresult').textContent;
-
-      !passed && printErrors();
-
-      console.log(colors[color](text));
-      return text;
-    }
-
-    function cleanUp() {
-      browser.close();
-      done();
-    }
-
-    function testFile(file) {
-      var url = 'file:/' + path.resolve(file);
-
-      console.log('Running ' + file);
-
-      browser.visit(url).then(printQunitResults).then(next);
-    }
-
-    function queueTest() {
-      setTimeout(function () {
-        testFile(testFiles.pop());
-      });
-    }
-
-    function next() {
-      if (testFiles.length) {
-        queueTest();
-      } else {
-        cleanUp();
-      }
-    }
-
-    next();
-  }
+  gulp.src('./src/img/**/*')
+    .pipe(gulp.dest('./build/img'));
 });
 
-// default is what runs when you just type "grunt" with no params
-gulp.task('default', ['build'], function() {
-  gulp.watch(paths.src + '/**/*.{js,html,handlebars}', ['reload-html']);
-  gulp.watch(paths.src + '/**/*.scss', ['reload-sass']);
+/* Process scss and copy to the css build directory */
+gulp.task('css', function() {
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
+});
+
+/* Process js and copy to the js build directory */
+gulp.task('js', function() {
+  return gulp.src('./src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('hbs', function() {
+  var hbs = {
+    content: {},
+    options: {
+      helpers: gulp.src('./src/helpers/**/*.js'),
+      partials: gulp.src('./src/partials/**/*.hbs')
+    }
+  };
+
+  return gulp.src(['./src/**/*.hbs', '!./src/partials/**/*.hbs'])
+    .pipe(handlebars(hbs.content, hbs.options))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest('./build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', function() {
+  return gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('uglify', function() {
+  return gulp.src('./build/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./release/js'))
+});
+
+gulp.task('minify', function() {
+  return gulp.src('./build/*.html')
+    .pipe(inlinesource({
+      compress: true
+    }))
+    .pipe(minifyhtml())
+    .pipe(hashSrc({
+      build_dir: './release',
+      src_path: './build'
+    }))
+    .pipe(gulp.dest('./release'))
+});
+
+/* clean cleans the build directory */
+gulp.task('cleanBuild', function(cb) {
+  return del('./build', cb);
+});
+
+/* clean cleans the release directory */
+gulp.task('cleanRelease', function(cb) {
+  return del('./release', cb);
+});
+
+/*
+  Default task, no optimizations, serves from the 'build'
+  directory
+*/
+gulp.task('default', ['serveBuild', 'cleanBuild', 'watch'], function() {
+  gulp.start('js', 'css', 'hbs', 'html');
+});
+
+/*
+  Release build, optimizes sources and serves from
+  the 'release' directory
+*/
+gulp.task('release', ['serveRelease', 'cleanRelease'], function() {
+  gulp.start('uncss', 'uglify', 'minify');
+});
+
+/* Watch task */
+gulp.task('watch', function() {
+  gulp.watch(['./src/fonts/**/*', './src/img/**/*'], ['assets']);
+  gulp.watch('./src/scss/**/*.scss', ['css']);
+  gulp.watch('./src/js/**/*.js', ['js']);
+  gulp.watch('./src/**/*.html', ['html']);
+  gulp.watch('./src/**/*.hbs', ['hbs']);
 });
